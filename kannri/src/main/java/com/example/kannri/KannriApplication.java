0e76e6bdaf8e78fc21978c7078a048592f059fa9
SnorosiS <norosi0201@gmail.com>
2024-09-01 23:31:45 +0900

    first commit

---
+++ kannri/src/main/java/com/example/kannri/KannriApplication.java
@@ -0,0 +1,13 @@
+package com.example.kannri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KannriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KannriApplication.class, args);
+	}
+
+}
